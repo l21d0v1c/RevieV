1de425849f69e3df707d3c4a680f1c3d8906af94
l21d0v1c <l21d0v1c.c1v0d21l@gmail.com>
2025-09-27 16:15:17 +0200

    Create cardBackgrounds.ts

---
+++ src/data/cardBackgrounds.ts
@@ -0,0 +1,6 @@
+export const cardBackgrounds = {
+  daily: '/images/card-backgrounds/daily-card-background.jpg',
+  monthly: '/images/card-backgrounds/monthly-card-background.jpg',
+  quarterly: '/images/card-backgrounds/quarterly-card-background.jpg',
+  yearly: '/images/card-backgrounds/yearly-card-background.jpg',
+};
